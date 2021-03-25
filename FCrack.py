ba4a75a56a54925ffba00302ff6c710bce2b343f
root <tarun.yenni@outlook.com>
2021-03-25 06:43:31 -0400

    adding new files

--- FCrack.py
+++ FCrack.py
@@ -18,28 +18,34 @@ print("Supported file formats: .zip, .rar, .pdf, .7z, .doc/.docx (2007/2013)")
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-f","--ifile", metavar="<Input_File>", required=True, help="Take's a file_name as input")
-parser.add_argument("-w","--wordlist", metavar="<wordlist>", required=True, help="Take's a wordlist as input")
+parser.add_argument("-w","--wordlist", metavar="<wordlist>", help="Take's a wordlist as input")
 
 args = parser.parse_args()
+
 
 #global file_name
 file_name = args.ifile
 wordlist = args.wordlist
 
 def crack_hash(wordlist):
-    os.system("john hash.txt " + "-w=" + wordlist)
+    if args.wordlist is True:
+        os.system("john hash.txt " + "-w=" + wordlist)
+    else:
+        os.system("john hash.txt -w=resources/wordlist/rockyou-50.txt")
+    if os.path.exists("hash.txt"):
+        os.system("rm hash.txt")
 
 def create_hash(file_name):
     if ".zip" in file_name:
         os.system("zip2john " + file_name + "> hash.txt")
     elif ".pdf" in  file_name:
-        os.system("programs/pdf2john.pl "+ file_name +"> hash.txt")
+        os.system("resources/pdf2john.pl "+ file_name +"> hash.txt")
     elif ".rar" in file_name:
         os.system("rar2john "+ file_name + "> hash.txt")
     elif (".doc" or ".docx") in file_name:
-        os.system("programs/office2john.py "+ file_name +">hash.txt")
+        os.system("resources/office2john.py "+ file_name +">hash.txt")
     elif ".7z" in file_name:
-        os.system("programs/7z2john.pl "+ file_name +"> hash.txt")
+        os.system("resources/7z2john.pl "+ file_name +"> hash.txt")
     else:
         print("File Format not supported!!!")
         print("Supported File formats : .zip, .rar, .pdf, .7z, .doc/docx (2007/2013)")
--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # FCracker
 
-**Current Release: v0.0.1 (2021.25.03)**
+**Current Release: v0.0.22(2021.25.03)**
 
 
 Overview
 --------
 - FCrack is a command-line tool designed to brute force encrypted files like zip, 7z, rar, pdf etc.
-- At present it works only in linux with proper depencences. This tool is made for both personal and CTF use.
+- At present it works only in linux with proper dependencies. This tool is made for both personal and CTF use.
 
 - FCrack is developed by [@zZBlackArrowZz](https://twitter.com/zZBlackArrowZz)
 
@@ -34,7 +34,7 @@ usage: FCrack.py [-h] -f <Input_File> -w <wordlist>
 FCrack.py: error: argument -f/--ifile is required
 ```
 
-Dependences
+Dependencies
 -----------
 ```
 sudo apt-get install john -y
